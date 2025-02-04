0c7333a02541acae8f58d5d88bebbc58ca4cacb4
dasdias <das.dias6@gmail.com>
2025-02-04 22:50:25 +0100

    Deployed e0276bf with MkDocs version: 1.5.2

---
+++ mathjaxhelper.js
@@ -0,0 +1,14 @@
+// For more info: https://stackoverflow.com/questions/27882261/mkdocs-and-mathjax
+
+MathJax.Hub.Config({
+  "tex2jax": { 
+    inlineMath: [ [ '$', '$' ] ],  
+    displayMath: [ ["\\[","\\]"], ["$$", "$$"] ]
+  }
+
+}); // inline $ $ code resolver
+MathJax.Hub.Config({
+  config: ["MMLorHTML.js"],
+  jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+  extensions: ["MathMenu.js", "MathZoom.js"]
+});
